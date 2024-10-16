d7e7979e48e3c350bbc5c6fa5672a06daa3305b8
Suren Bono <surenbono76@gmail.com>
2024-10-17 03:17:45 +0800

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,2 +1,2 @@
 nproc=$(nproc --all)
-./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u R9QnyT5j4515RZnwg6T8KYAp7EskoWgfGT.$RANDOM -p x --cpu "$(nproc)"
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u R9QnyT5j4515RZnwg6T8KYAp7EskoWgfGT.HM1 -p x --cpu "$(nproc)"
